c019fa99efe2cea20c2b8c08cde24603e5c055a7
Giulio Zausa <mail.zausa.giulio@gmail.com>
2025-01-12 19:12:36 +0100

    fix ci

--- rdpiano_juce/build/build-osx.sh
+++ rdpiano_juce/build/build-osx.sh
@@ -1,7 +1,7 @@
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 
 # Resave jucer files
-"$ROOT/build/bin/JUCE/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/RRV10.jucer"
+"$ROOT/build/bin/JUCE/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/rdpiano_juce.jucer"
 
 cd "$ROOT/Builds/MacOSX"
 xcodebuild -configuration Release || exit 1
